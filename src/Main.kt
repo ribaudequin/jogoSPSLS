9c4218a5472c2d235925e49c37e2f1a0d6747e99
ribaudequin <esimarsa@gmail.com>
2024-09-11 23:19:36 +0100

    11/09/2024

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,42 @@
 fun main() {
-    println("Hello World!")
+    menu()
+}
+
+
+
+fun menu () {
+    println("##########################################################")
+    println("# Bem vindo ao jogo Papel, Pedra, Tesoura Lagarto, Spock #")
+    println("##########################################################")
+    println("#         Escolha uma das opções abaixo:                 #")
+    println("##########################################################")
+    println("# 1 - Regras do jogo                                     #")
+    println("# 2 - Iniciar o jogo                                     #")
+    println("# 3 - Sair                                               #")
+    println("##########################################################")
+
+    val opcao = readlnOrNull()
+
+    when (opcao) {
+        "1" -> regras()
+        "2" -> iniciarJogo()
+        "3" -> sair()
+        else -> {
+            println("Opcão inválida. Pressione ENTER para tentar novamente.")
+            readln()
+            menu()
+        }
+    }
+}
+
+fun regras() {
+    // TODO:
+}
+
+fun iniciarJogo() {
+    // TODO:
+}
+
+fun sair() {
+    return
 }
