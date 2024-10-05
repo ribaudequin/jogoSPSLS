d4c0f989ee23f1c36a23547d590c8557986c6a40
ribaudequin <esimarsa@gmail.com>
2024-10-05 18:41:02 +0100

    17/09/2024

--- src/Main.kt
+++ src/Main.kt
@@ -13,7 +13,8 @@ fun menu() {
     println("# 2 - Iniciar o jogo                                     #")
     println("# 3 - Sair                                               #")
     println("##########################################################")
-    println("#                    ${simbolos.Papel.code} ${simbolos.Pedra.code} ${simbolos.Tesoura.code} ${simbolos.Lagarto.code} ${simbolos.Spock.code}                      #")
+    println("#                    ${simbolos.papel.code} ${simbolos.pedra.code} " +
+            "${simbolos.tesoura.code} ${simbolos.lagarto.code} ${simbolos.spock.code}                      #")
 
     val opcao = readlnOrNull()
 
@@ -49,7 +50,7 @@ fun regras() {
 
 fun iniciarJogo() {
     println("Escolha Papel, Pedra, Tesoura, Lagarto ou Spock")
-    val escolhaJogador = readlnOrNull()   // lembrar para por em lowercase  <---------- IMPORTANTE
+    val escolhaJogador = readlnOrNull()?.lowercase()   // lembrar para por em lowercase  <---------- IMPORTANTE
 
     if (escolhaJogador == null) {
         println("Opcão inválida. Pressione ENTER para tentar novamente.")
@@ -76,29 +77,38 @@ fun voltaOuMenu() {
 fun mensagem(opcao1: String, opcao2: String): String {
     when {
         opcao1 == "tesoura" && opcao2 == "papel" || opcao1 == "papel" && opcao2 == "tesoura" ->
-            return "Tesoura ${simbolos.Tesoura.code} corta papel ${simbolos.Papel.code}"
+            return "Tesoura ${simbolos.tesoura.code} corta papel ${simbolos.papel.code}"
         opcao1 == "papel" && opcao2 == "pedra" || opcao1 == "pedra" && opcao2 == "papel" ->
-            return "Papel ${simbolos.Papel.code} cobre pedra ${simbolos.Pedra.code}"
-        opcao1 == "pedra" && opcao2 == "lagarto" || opcao1 == "lagarto" && opcao2 == "pedra" -> return "Pedra esmaga lagarto"
-        opcao1 == "lagarto" && opcao2 == "spock" || opcao1 == "spock" && opcao2 == "lagarto" -> return "Lagarto envenena Spock"
-        opcao1 == "spock" && opcao2 == "tesoura" || opcao1 == "tesoura" && opcao2 == "spock" -> return "Spock Vaporiza tesoura"
-        opcao1 == "tesoura" && opcao2 == "lagarto" || opcao1 == "lagarto" && opcao2 == "tesoura" -> return "Tesoura decapita lagarto"
-        opcao1 == "lagarto" && opcao2 == "papel" || opcao1 == "papel" && opcao2 == "lagarto" -> return "Lagarto come papel"
-        opcao1 == "papel" && opcao2 == "spock" || opcao1 == "spock" && opcao2 == "papel" -> return "Papel desmente Spock"
-        opcao1 == "spock" && opcao2 == "pedra" || opcao1 == "pedra" && opcao2 == "spock" -> return "Spock destrói pedra"
-        opcao1 == "pedra" && opcao2 == "tesoura" || opcao1 == "tesoura" && opcao2 == "pedra" -> return "Pedra parte tesoura"
-        else -> return "Empate"
+            return "Papel ${simbolos.papel.code} cobre pedra ${simbolos.pedra.code}"
+        opcao1 == "pedra" && opcao2 == "lagarto" || opcao1 == "lagarto" && opcao2 == "pedra" ->
+            return "Pedra ${simbolos.pedra.code} esmaga lagarto ${simbolos.lagarto.code}"
+        opcao1 == "lagarto" && opcao2 == "spock" || opcao1 == "spock" && opcao2 == "lagarto" ->
+            return "Lagarto ${simbolos.lagarto.code} envenena Spock ${simbolos.spock.code}"
+        opcao1 == "spock" && opcao2 == "tesoura" || opcao1 == "tesoura" && opcao2 == "spock" ->
+            return "Spock  ${simbolos.spock.code} Vaporiza tesoura ${simbolos.tesoura.code}"
+        opcao1 == "tesoura" && opcao2 == "lagarto" || opcao1 == "lagarto" && opcao2 == "tesoura" ->
+            return "Tesoura ${simbolos.tesoura.code} decapita lagarto ${simbolos.lagarto.code}"
+        opcao1 == "lagarto" && opcao2 == "papel" || opcao1 == "papel" && opcao2 == "lagarto" ->
+            return "Lagarto ${simbolos.lagarto.code} come papel ${simbolos.papel.code}"
+        opcao1 == "papel" && opcao2 == "spock" || opcao1 == "spock" && opcao2 == "papel" ->
+            return "Papel ${simbolos.papel.code} desmente Spock ${simbolos.spock.code}"
+        opcao1 == "spock" && opcao2 == "pedra" || opcao1 == "pedra" && opcao2 == "spock" ->
+            return "Spock ${simbolos.spock.code} destrói pedra ${simbolos.pedra.code}"
+        opcao1 == "pedra" && opcao2 == "tesoura" || opcao1 == "tesoura" && opcao2 == "pedra" ->
+            return "Pedra ${simbolos.pedra.code} parte tesoura ${simbolos.tesoura.code}"
+        else -> return "Empate ${simbolos.empate.code}"
     }
 }
 
 
 
 enum class simbolos(val code: String) {
-    Papel("\uD83D\uDCF0"),
-    Pedra("\uD83E\uDEA8"),
-    Tesoura("✂\uFE0F"),
-    Lagarto("\uD83E\uDD8E"),
-    Spock("\uD83D\uDD96");
+    papel("\uD83D\uDCF0"),
+    pedra("\uD83E\uDEA8"),
+    tesoura("✂\uFE0F"),
+    lagarto("\uD83E\uDD8E"),
+    spock("\uD83D\uDD96"),
+    empate("\uD83D\uDFF0");
 }
 
 
--- src/logica.kt
+++ src/logica.kt
@@ -7,8 +7,26 @@ fun gerarComputador(): String {
 }
 
 fun quemGanhou(jogador: String, computador: String) {
-    // TODO: adicionar o resto da logica
     println(mensagem(jogador, computador))
-    readln()
+
+
+    if (jogador == computador) {
+        println("O jogador empata ${simbolos.empate.code} com o computador")
+    } else if (jogador == "pedra" && (computador == "tesoura" || computador == "lagarto")) {
+        println("O jogador ganhou ${simbolos.pedra.code} ao computador")
+    } else if (jogador == "papel" && (computador == "pedra" || computador == "spock")) {
+        println("O jogador ganhou ${simbolos.papel.code} ao computador")
+    } else if (jogador == "tesoura" && (computador == "papel" || computador == "lagarto")) {
+        println("O jogador ganhou ${simbolos.tesoura.code} ao computador")
+    } else if (jogador == "lagarto" && (computador == "spock" || computador == "papel")) {
+        println("O jogador ganhou ${simbolos.lagarto.code} ao computador")
+    } else if (jogador == "spock" && (computador == "tesoura" || computador == "pedra")) {
+        println("O jogador ganhou ${simbolos.spock.code} ao computador")
+    } else {
+        println("O computador ganhou ${simbolos.valueOf(computador).code} ao Jogador")
+    }
+
     voltaOuMenu()
 }
+
+
